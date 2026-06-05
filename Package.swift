72f44b2c1baf48eafbf38c6a5e7c034d74c1d159
nmarkman <nmarkman93@gmail.com>
2026-06-05 15:16:01 -0400

    restore: add KeyboardShortcuts dependency back to Package.swift

--- Package.swift
+++ Package.swift
@@ -4,11 +4,17 @@ import PackageDescription
 let package = Package(
     name: "Sniploop",
     platforms: [.macOS(.v14)],
+    dependencies: [
+        .package(url: "https://github.com/sindresorhus/KeyboardShortcuts", from: "2.0.0"),
+    ],
     targets: [
         .target(name: "SniploopCore"),
         .executableTarget(
             name: "Sniploop",
-            dependencies: ["SniploopCore"]
+            dependencies: [
+                "SniploopCore",
+                .product(name: "KeyboardShortcuts", package: "KeyboardShortcuts"),
+            ]
         ),
         .testTarget(name: "SniploopCoreTests", dependencies: ["SniploopCore"]),
     ]

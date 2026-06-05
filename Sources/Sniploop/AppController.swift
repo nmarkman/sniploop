dd5506a07f3a3aaf0f8f32e56f11117d3ec3c364
nmarkman <nmarkman93@gmail.com>
2026-06-05 16:03:23 -0400

    feat(app): global Hyper-chord hotkey via Carbon RegisterEventHotKey

--- Sources/Sniploop/AppController.swift
+++ Sources/Sniploop/AppController.swift
@@ -3,6 +3,7 @@ import SniploopCore
 
 final class AppController: NSObject, NSApplicationDelegate {
     private var menuBar: MenuBarController?
+    private var hotKey: GlobalHotKey?
 
     func applicationDidFinishLaunching(_ note: Notification) {
         NSApp.setActivationPolicy(.accessory)
@@ -10,6 +11,7 @@ final class AppController: NSObject, NSApplicationDelegate {
         mb.onNewCapture = { [weak self] in self?.startCapture() }
         mb.onQuit = { NSApp.terminate(nil) }
         menuBar = mb
+        hotKey = GlobalHotKey.defaultCapture { [weak self] in self?.startCapture() }
     }
 
     func startCapture() {
---
+++ Sources/Sniploop/GlobalHotKey.swift
@@ -0,0 +1,38 @@
+import AppKit
+import Carbon.HIToolbox
+
+/// A process-global hotkey via Carbon's RegisterEventHotKey. Global without the Accessibility
+/// permission that NSEvent global monitors require, and with no third-party dependency.
+final class GlobalHotKey {
+    private var hotKeyRef: EventHotKeyRef?
+    private var eventHandler: EventHandlerRef?
+    var onFire: (() -> Void)?
+
+    /// `keyCode` is a Carbon virtual key code (e.g. kVK_ANSI_G); `modifiers` combine
+    /// cmdKey / controlKey / optionKey / shiftKey.
+    init(keyCode: UInt32, modifiers: UInt32) {
+        var eventType = EventTypeSpec(eventClass: OSType(kEventClassKeyboard), eventKind: UInt32(kEventHotKeyPressed))
+        InstallEventHandler(GetApplicationEventTarget(), { _, _, userData in
+            guard let userData else { return noErr }
+            Unmanaged<GlobalHotKey>.fromOpaque(userData).takeUnretainedValue().onFire?()
+            return noErr
+        }, 1, &eventType, Unmanaged.passUnretained(self).toOpaque(), &eventHandler)
+
+        let id = EventHotKeyID(signature: OSType(0x534E4C50), id: 1) // 'SNLP'
+        RegisterEventHotKey(keyCode, modifiers, id, GetApplicationEventTarget(), 0, &hotKeyRef)
+    }
+
+    deinit {
+        if let hotKeyRef { UnregisterEventHotKey(hotKeyRef) }
+        if let eventHandler { RemoveEventHandler(eventHandler) }
+    }
+
+    /// The default Sniploop trigger: Hyper (Cmd-Ctrl-Opt-Shift) + G. A Caps-Lock-as-Hyper
+    /// setup (via Raycast/Karabiner) sends this same chord.
+    static func defaultCapture(onFire: @escaping () -> Void) -> GlobalHotKey {
+        let hk = GlobalHotKey(keyCode: UInt32(kVK_ANSI_G),
+                              modifiers: UInt32(cmdKey | controlKey | optionKey | shiftKey))
+        hk.onFire = onFire
+        return hk
+    }
+}
